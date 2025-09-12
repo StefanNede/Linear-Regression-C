ca22748bdcfac26cfc207ce4bcda1669f24003b4
Stefan Nedelcu <stefan.n@gmx.co.uk>
2025-09-12 15:01:40 +0100

    Correctly extracting the number of dimensions we are working in from input data file

--- multi.c
+++ multi.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "multi.h"
 
 /* VARIABLES with types
     p - int - number of dimensions we are running regression on
@@ -18,7 +21,94 @@
  => R * b = Q_T * y (solve via back sub)
 */
 
-int main(void) {
+// GLOBALS -------------------------------
+volatile int n, p;
+
+// FUNCTIONS -------------------------------
+
+// Count the number of lines in an input file
+void set_lines_dimensions(char *filename) {
+    FILE *fptr; 
+    char c;
+    int count = 0; 
+    int dimensions = 1;
+
+    fptr = fopen(filename, "r");
+
+    if (fptr == NULL) {
+        printf("Failed at counting lines for file: `%s`\n", filename);
+        return;
+    }
+
+    for (c = getc(fptr); c != EOF; c = getc(fptr)) {
+        if (c == ',') {
+            dimensions++;
+        }
+
+        if (c == '\n') { 
+            count++; 
+            dimensions = 1;
+        }
+    }
+
+    fclose(fptr);
+    n = count+1;
+    p = dimensions;
+}
+
+// Read the data input from the csv file
+DataInputs read_data(void) {
+    FILE *fptr;  
+    DataInputs data_inputs;
+    char c, line[1000];
+    int onx = 1, i = 0; // flag: 1 - on x field of input, 0 - on y field of input
+    double x, y;
+
+    data_inputs.x_inputs.n= n;
+    data_inputs.y_inputs.size = n;
+    data_inputs.x_inputs.data = (double*)malloc(n*sizeof(double));
+    data_inputs.y_inputs.data = (double*)malloc(n*sizeof(double));
+
+    fptr = fopen("data.txt", "r");
+
+    if (fptr != NULL) {
+        while (fgets(line, sizeof(line), fptr)) {
+            if (sscanf(line, "%lf,%lf", &x, &y) == 2) {
+                data_inputs.x_inputs.data[i] = x;
+                data_inputs.y_inputs.data[i] = y;
+                i++;
+            } else {
+                fprintf(stderr, "Invalid line format: %s", line);
+            }
+        }
+    } else {
+        printf("Not able to open the file: `data.txt`\n");
+    }
+
+    fclose(fptr);
+
+    return data_inputs;
+}
+
+void multiple_regression(void) {
     printf("Running Multiple Linear Regression on Input from `data.txt`\n");
+
+    // Loading in data 
+    set_lines_dimensions("data.txt");
+    printf("Number of datapoints: %d\n", n);
+    printf("Number of dimensions: %d\n", p);
+
+    // DataInputs data_inputs = read_data();
+
+
+    // PERFORM SIMPLE LINEAR REGRESSION ===========
+
+    // Free used memory
+    // free(data_inputs.x_inputs.data);
+    // free(data_inputs.y_inputs.data);
+}
+
+int main(void) {
+    multiple_regression();
     return 0;
 }
---
+++ multi.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// STRUCTS
+struct Vector;
+typedef struct Vector Vector;
+
+struct Matrix;
+typedef struct Matrix Matrix;
+
+struct DataInputs;
+typedef struct DataInputs DataInputs;
+
+// Struct for a size x 1 vector
+struct Vector {
+    double* data;
+    int size; 
+};
+
+// Struct for an nxm matrix
+// could also implement as a list of pointers 
+struct Matrix {
+    int n, m; 
+    double* data; // data[i][j] = data[i*n + m]
+};
+
+// Struct for the 2 vector inputs of x and y values
+struct DataInputs {
+    struct Matrix x_inputs;
+    struct Vector y_inputs;
+};
+
+// FUNCTION DECLARATIONS
+void set_lines_dimensions(char *filename);

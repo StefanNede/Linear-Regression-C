5c71d3aace196b052ad0eaf60e07294509dee4de
Stefan Nedelcu <stefan.n@gmx.co.uk>
2025-10-01 16:49:25 +0100

    Fixed 3d grid generation to correct interval for x,y values

--- main.py
+++ main.py
@@ -44,16 +44,28 @@ def plot_3d():
     assert len(datapoints[0]) == 3 
     assert len(plane_coefficients) == 3
 
+    # Define the 3D points
+    # Example points (x, y, z)
+    points = np.array(datapoints)
+
+    x_points = points[:, 0]
+    y_points = points[:, 1]
+    z_points = points[:, 2]
+
     # The plane equation is Cz = D + Ax + By where C = 1
     A = plane_coefficients[1]
     B = plane_coefficients[2]
     C = 1
     D = plane_coefficients[0]
 
-    # TODO: Fix this range of values (find min and max x,y values)
     # Create a grid of x and y values
-    x = np.linspace(-20, 20, 40)
-    y = np.linspace(-20, 20, 40)
+    min_x, max_x = min(x_points), max(x_points)
+    deviation_x = abs(max_x - min_x)
+    min_y, max_y = min(y_points), max(y_points)
+    deviation_y = abs(max_y - min_y) # use this to give boundary around points for nicer plotting
+
+    x = np.linspace(min_x - deviation_x, max_x + deviation_x)
+    y = np.linspace(min_y - deviation_y, max_y + deviation_y)
     X, Y = np.meshgrid(x, y)
 
     # Calculate the corresponding z values
@@ -64,27 +76,20 @@ def plot_3d():
 
     # Create the 3D plot
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(111, projection="3d")
 
     # Plot the surface -> alpha=0.5 to make plane semi transparent to allow us to see the points
-    ax.plot_surface(X, Y, Z, alpha=0.5, cmap='viridis')
+    ax.plot_surface(X, Y, Z, alpha=0.5, cmap="viridis")
 
-    # Define and plot the 3D points
-    # Example points (x, y, z)
-    points = np.array(datapoints)
-
-    # Plot the points
-    x_points = points[:, 0]
-    y_points = points[:, 1]
-    z_points = points[:, 2]
-    ax.scatter(x_points, y_points, z_points, color='red', s=10, label='Data Points')
+    # Plot the 3D points
+    ax.scatter(x_points, y_points, z_points, color="red", s=10, label="Data Points")
 
     # Set labels for the axes
-    ax.set_xlabel('X axis')
-    ax.set_ylabel('Y axis')
-    ax.set_zlabel('Z axis')
+    ax.set_xlabel("X axis")
+    ax.set_ylabel("Y axis")
+    ax.set_zlabel("Z axis")
 
-    # TODO: fix this title
+    # TODO: make this title nicer
     plt.title(f"3D Plane: {A}x + {B}y - {C}z = -{D}")
     plt.show()
 
